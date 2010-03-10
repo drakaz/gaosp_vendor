2efc15a92ff4ab7a035cfc8cc2cdd3bb4c68de65
drakaz <drakpub@gmail.com>
2010-03-10 14:45:01 +0100

    Build process reviewed with build script, source file, help

--- build_repo.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-source build/envsetup.sh
-lunch I7500-eng
-make clean
-make
--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+curl http://android.git.kernel.org/repo > repo
+chmod 755 repo
 cd .repo
 rm -f local_manifest.xml
 wget http://gaosp.googlecode.com/files/local_manifest.xml
 cd ..
-repo sync
+./repo sync
