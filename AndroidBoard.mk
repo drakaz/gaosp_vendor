352ffdfc3d2d1cb4149686b58485f4bce235e660
drakaz <drakpub@gmail.com>
2010-06-19 15:46:59 +0200

    Add some binaries

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -174,6 +174,50 @@ ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/open/bin/rc | $(ACP)
 	$(transform-prebuilt-to-target)
 
+file := $(TARGET_OUT)/xbin/remountro
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/remountro | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/remountrw
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/remountrw | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/rmmod
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/rmmod | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/rzcontrol
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/rzcontrol | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/scp
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/scp | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/sqlite3
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/sqlite3 | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/ssh
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/ssh | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/vim
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/vim | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_OUT)/xbin/zsh
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/open/bin/zsh | $(ACP)
+	$(transform-prebuilt-to-target)
 
 ###########
 # busybox #
