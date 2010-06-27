533399a65c0996ca74a523204c2fe3ad0e266032
drakaz <drakpub@gmail.com>
2010-06-27 21:34:08 +0200

    Remove libOmxCore. build from source (nopy)

--- qcom/proprietary-open/dsp/mm/unzip-files.sh
+++ qcom/proprietary-open/dsp/mm/unzip-files.sh
@@ -2,5 +2,5 @@
 
 mkdir -p proprietary
 
-unzip -j -o ../../../../$1 system/lib/libmm-adspsvc.so system/lib/libOmxCore.so system/lib/libOmxH264Dec.so system/lib/libOmxMpeg4Dec.so system/lib/libOmxVidEnc.so -d proprietary
+unzip -j -o ../../../../$1 system/lib/libmm-adspsvc.so system/lib/libOmxH264Dec.so system/lib/libOmxMpeg4Dec.so system/lib/libOmxVidEnc.so -d proprietary
 touch proprietary/*
