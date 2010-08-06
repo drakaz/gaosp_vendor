872a03053ada2e12aa3b02781ba4f28fe98f9fd2
Nicolas PINOT <npinot@gmail.com>
2010-08-06 22:32:45 +0800

    Add required camera libraries

--- qcom/proprietary-open/dsp/libqcamera/unzip-files.sh
+++ qcom/proprietary-open/dsp/libqcamera/unzip-files.sh
@@ -2,5 +2,5 @@
 
 mkdir -p proprietary
 
-unzip -j -o ../../../../$1 system/lib/libcamera.so system/lib/libcameraservice.so -d proprietary
+unzip -j -o ../../../../$1 system/lib/libcamera.so system/lib/libmmcamera.so system/lib/libmm-qcamera-tgt.so system/lib/libmmjpeg.so -d proprietary
 touch proprietary/*
