ac1727e64c6d85afe3b50c7b48fb9852df520555
drakaz <drakpub@gmail.com>
2010-10-21 21:14:45 +0200

    Added YAAM market and fix firstboot issue
    Change-Id: I3e8d31c6ff6471fea701cbf8807a09206ad12b0e

--- open/bin/firstboot.sh
+++ open/bin/firstboot.sh
@@ -1,5 +1,11 @@
 #!/system/bin/sh
 
+# Wait for settings.db to be created
+while [ ! -e /dbdata/databases/com.android.providers.settings/settings.db ]
+do
+	sleep 1
+done
+sleep 5
 # System default settings
 /system/xbin/sqlite3 /dbdata/databases/com.android.providers.settings/settings.db "INSERT INTO system (name, value) VALUES ('trackball_wake_screen', 1);" 
 /system/xbin/sqlite3 /dbdata/databases/com.android.providers.settings/settings.db "INSERT INTO system (name, value) VALUES ('trackball_unlock_screen', 1);" 
--- scripts/mkupdate.sh
+++ scripts/mkupdate.sh
@@ -9,6 +9,10 @@ mv out/target/product/I7500/system/app out/target/product/I7500/dev_data/app_s
 # Addin firstboot.sh script to /data
 cp -p device/Samsung/I7500/open/bin/firstboot.sh out/target/product/I7500/dev_data/firstboot.sh
 
+# Addons
+mkdir out/target/product/I7500/dev_data/app/
+#wget -O out/target/product/I7500/dev_data/app/yaam.apk http://yaam.mobi/apk/yaam.apk
+
 ### For future kernel generation, for now we just get prebuild boot.img from gcode download page
 #rm -rf out/target/product/I7500/ramdisk.gz
 #cp -rp device/Samsung/I7500/open/boot/init out/target/product/I7500/root/init
@@ -26,11 +30,11 @@ find  out/target/product/I7500/system/bin/ -type l -exec rm "{}" \;
 cd out/target/product/I7500/
 
 ### zip and sign
-zip -qyr unsigned.zip META-INF data system boot.img dev_data
+zip -qyr unsigned.zip META-INF data system boot.img dev_data yaam.apk
 java -jar ../../../host/linux-x86/framework/signapk.jar ../../../../build/target/product/security/testkey.x509.pem ../../../../build/target/product/security/testkey.pk8 unsigned.zip update.zip
 
 ### Restore original directory for data/app_s
 cd ../../../../
 mv out/target/product/I7500/dev_data/app_s out/target/product/I7500/system/app
 rm out/target/product/I7500/dev_data/firstboot.sh
-rmdir out/target/product/I7500/dev_data/
+#rm -rf out/target/product/I7500/dev_data/
