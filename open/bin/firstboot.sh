4be41666e7da61da5a38b2fc3642af11ab2dd2f1
drakaz <drakpub@gmail.com>
2010-10-24 22:47:40 +0200

    Fix databases location in firstboot.sh
    Change-Id: I567a87c8171c39290441e23071a12254e4c26110

--- open/bin/firstboot.sh
+++ open/bin/firstboot.sh
@@ -1,15 +1,17 @@
 #!/system/bin/sh
 
+DATABASE="/data/data/com.android.providers.settings/databases/settings.db"
+
 # Wait for settings.db to be created
-while [ ! -e /dbdata/databases/com.android.providers.settings/settings.db ]
+while [ ! -e $DATABASE ]
 do
 	sleep 1
 done
 sleep 5
 # System default settings
-/system/xbin/sqlite3 /dbdata/databases/com.android.providers.settings/settings.db "INSERT INTO system (name, value) VALUES ('trackball_wake_screen', 1);" 
-/system/xbin/sqlite3 /dbdata/databases/com.android.providers.settings/settings.db "INSERT INTO system (name, value) VALUES ('trackball_unlock_screen', 1);" 
-/system/xbin/sqlite3 /dbdata/databases/com.android.providers.settings/settings.db "INSERT INTO system (name, value) VALUES ('lock_home_in_memory', 1);"
+/system/xbin/sqlite3 $DATABASE "INSERT INTO system (name, value) VALUES ('trackball_wake_screen', 1);" 
+/system/xbin/sqlite3 $DATABASE "INSERT INTO system (name, value) VALUES ('trackball_unlock_screen', 1);" 
+/system/xbin/sqlite3 $DATABASE "INSERT INTO system (name, value) VALUES ('lock_home_in_memory', 1);"
 # Secure default settings
-/system/xbin/sqlite3 /dbdata/databases/com.android.providers.settings/settings.db "INSERT INTO secure (name, value) VALUES ('install_non_market_apps', 1);"
-/system/xbin/sqlite3 /dbdata/databases/com.android.providers.settings/settings.db "INSERT INTO secure (name, value) VALUES ('allow_move_all_apps_external', 1);"
+/system/xbin/sqlite3 $DATABASE "INSERT INTO secure (name, value) VALUES ('install_non_market_apps', 1);"
+/system/xbin/sqlite3 $DATABASE "INSERT INTO secure (name, value) VALUES ('allow_move_all_apps_external', 1);"
