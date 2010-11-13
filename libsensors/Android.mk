27c6d2c1a24a583bb42d5310612c669d2c7fa80a
bukington <bukington@gmail.com>
2010-11-13 21:35:07 +0100

    Initial import of libsensors from https://github.com/OpenEtna/android_device_lg_eve
    Change-Id: Ica70b89efbba07dbc3a40ce3e2584077d711ffbd

---
+++ libsensors/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+#LOCAL_STATIC_LIBRARIES := libcutils libc libAK8973
+#ifeq ($(TARGET_DEVICE),surf)
+#LOCAL_SRC_FILES += sensors_atos.c
+#LOCAL_SRC_FILES += sensors_mma.c
+LOCAL_SRC_FILES += sensors_ak8973.c
+#LOCAL_SRC_FILES += sensors_proximity.c 
+#endif
+LOCAL_MODULE := sensors.samsung
+include $(BUILD_SHARED_LIBRARY)
+
