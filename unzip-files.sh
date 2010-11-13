c51ad9f4560bed39d31a160f4c85065fd042b137
bukington <bukington@gmail.com>
2010-11-13 22:31:35 +0100

    Update unzip script to use akmd2 from VT10.zip
    Change-Id: I882e6bbd4e35ddfa6a9b59767fe3d69042bcf2b4

--- unzip-files.sh
+++ unzip-files.sh
@@ -8,6 +8,7 @@ TAR=$1
 fi
 ZIP="I7500-PDA.zip"
 ZIP2="CM.zip"
+ZIP3="V10T.zip"
 
 mkdir -p tmp/system/
 mkdir proprietary
@@ -25,7 +26,6 @@ else
 fi
 	
 cd tmp/system/ && tar -xvf ../../$TAR system -C . && ../../$UNYAFFS_BIN system && rm -rf system && cd ../ && zip -r ../$ZIP system && cd ../ && rm -rf tmp/
-unzip -j -o $ZIP system/bin/akmd2 -d proprietary/bin/
 unzip -j -o $ZIP system/bin/vold -d proprietary/bin/
 unzip -j -o $ZIP system/bin/BCM4325D1* -d proprietary/bin/
 unzip -j -o $ZIP system/lib/libsec-ril.so -d proprietary/lib/
@@ -42,6 +42,11 @@ unzip -j -o $ZIP2 system/lib/libpvasfcommon.so -d proprietary/lib/
 unzip -j -o $ZIP2 system/lib/libpvasflocalpbreg.so -d proprietary/lib/
 unzip -j -o $ZIP2 system/lib/libpvasflocalpb.so -d proprietary/lib/
 unzip -j -o $ZIP2 system/etc/pvasflocal.cfg -d proprietary/
+
+# Akmd2 from VT10
+mkdir -p tmp/system && cd tmp/system/ && unzip ../../$ZIP3 system.img && ../../$UNYAFFS_BIN system.img && cd ../..
+cp tmp/system/bin/akmd2 proprietary/bin/
+rm -Rf tmp/
 
 (cd qcom/proprietary-open/loc_api && ./unzip-files.sh $ZIP)
 (cd qcom/proprietary-open/dsp/libqcamera && ./unzip-files.sh $ZIP)
