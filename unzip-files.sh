aab8943635cadf5f7395a01e3ada87ba41548974
bukington <bukington@gmail.com>
2010-11-19 12:44:50 +0100

    Remove lights.samsung.so library, lights.msm7k.so handle brightness interface
    Change-Id: I31a556e3d1c852800ae389302705d13a0c96ee6b

--- unzip-files.sh
+++ unzip-files.sh
@@ -30,7 +30,6 @@ unzip -j -o $ZIP system/bin/BCM4325D1* -d proprietary/bin/
 unzip -j -o $ZIP system/lib/libsec-ril.so -d proprietary/lib/
 unzip -j -o $ZIP system/lib/libloc_api.so -d proprietary/lib/
 unzip -j -o $ZIP system/lib/hw/copybit.samsung.so -d proprietary/lib/hw/
-unzip -j -o $ZIP system/lib/hw/lights.samsung.so -d proprietary/lib/hw/
 unzip -j -o $ZIP system/etc/rtecdc.bin -d proprietary/etc/
 unzip -j -o $ZIP system/usr/keychars/I7500_keypad.kcm.bin -d proprietary/keychars/
 
