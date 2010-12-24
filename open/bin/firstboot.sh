288cc8e869b56d0b089d3ba6aef128dd98a07931
drakaz <drakpub@gmail.com>
2010-12-24 11:53:49 +0100

    Force wifi sleep policy, avoiding freeze
    Change-Id: I06d64d4873eb0c53430f0f1ce052211e1bd22ad2

--- open/bin/firstboot.sh
+++ open/bin/firstboot.sh
@@ -12,6 +12,7 @@ sleep 5
 /system/xbin/sqlite3 $DATABASE "INSERT INTO system (name, value) VALUES ('trackball_wake_screen', 1);" 
 /system/xbin/sqlite3 $DATABASE "INSERT INTO system (name, value) VALUES ('trackball_unlock_screen', 1);" 
 /system/xbin/sqlite3 $DATABASE "INSERT INTO system (name, value) VALUES ('lock_home_in_memory', 1);"
+/system/xbin/sqlite3 $DATABASE "INSERT INTO system (name, value) VALUES ('wifi_sleep_policy', 2);"
 # Secure default settings
 /system/xbin/sqlite3 $DATABASE "INSERT INTO secure (name, value) VALUES ('install_non_market_apps', 1);"
 /system/xbin/sqlite3 $DATABASE "INSERT INTO secure (name, value) VALUES ('allow_move_all_apps_external', 1);"
