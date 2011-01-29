f215c64145d95d0167ade4acc10fb122fe5ee772
bukington <bukington@gmail.com>
2011-01-29 16:30:12 +0100

    Build I7500 kernel.
    Change-Id: I306457bdb3824a23dce73e0314ee0bf29d60ffea

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,9 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-$(transform-prebuilt-to-target)
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # Keylayouts
 file := $(TARGET_OUT_KEYLAYOUT)/I7500_headset.kl
