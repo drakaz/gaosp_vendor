9f63968a54e54ad5ee71f4335a2147ce5d809a56
bukington <bukington@gmail.com>
2011-01-29 16:30:12 +0100

    Build I7500 kernel.
    Change-Id: I306457bdb3824a23dce73e0314ee0bf29d60ffea

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 #Android makefile to build kernel as a part of Android Build
-#Its here and not under kernel/ for compatiblity with the froyo branch
-#The config for the kernel is arch/arm/configs/openetna
-LOCAL_PATH := $(call my-dir)/../../../kernel
+#Its here and not under gaosp-kernel/ for compatiblity with the froyo branch
+#The config for the kernel is arch/arm/configs/gaosp_msm_defconfig
+LOCAL_PATH := $(call my-dir)/../../../gaosp-kernel
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
@@ -12,8 +12,8 @@ $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
 
-$(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/openetna | $(ACP)
+$(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/gaosp_msm_defconfig | $(ACP)
 	$(transform-prebuilt-to-target)
 
 $(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- INSTALL_MOD_STRIP=1
+	$(MAKE) -C gaosp-kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- INSTALL_MOD_STRIP=1
