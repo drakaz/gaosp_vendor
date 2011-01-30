c82b3b9e5f9a49e9b57928fcd56ca1498a0a2d3f
bukington <bukington@gmail.com>
2011-01-30 15:36:09 +0100

    Add gingerbread libsensor from passion
    Change-Id: Ia510e20f95683483600c9f671ad197d5c2c35128

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -1,16 +1,45 @@
-LOCAL_PATH:= $(call my-dir)
+# Copyright (C) 2008 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(TARGET_SIMULATOR),true)
+
+# HAL module implemenation, not prelinked, and stored in
+# hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
+
+LOCAL_MODULE := sensors.mahimahi
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-#LOCAL_STATIC_LIBRARIES := libcutils libc libAK8973
-#ifeq ($(TARGET_DEVICE),surf)
-#LOCAL_SRC_FILES += sensors_atos.c
-#LOCAL_SRC_FILES += sensors_mma.c
-LOCAL_SRC_FILES += sensors_ak8973.c
-#LOCAL_SRC_FILES += sensors_proximity.c 
-#endif
-LOCAL_MODULE := sensors.samsung
+
 LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\"
+LOCAL_SRC_FILES := 						\
+				sensors.c 				\
+				nusensors.cpp 			\
+				InputEventReader.cpp	\
+				SensorBase.cpp			\
+				LightSensor.cpp			\
+				ProximitySensor.cpp		\
+				AkmSensor.cpp
+				
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_PRELINK_MODULE := false
+
 include $(BUILD_SHARED_LIBRARY)
 
+endif # !TARGET_SIMULATOR
