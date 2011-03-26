f4734710e6873f45783f2d2f615d71e813edc994
bukington <bukington@gmail.com>
2011-03-26 14:17:06 +0100

    Build cm-kernel
    Change-Id: I4460f17f7a89e273e8a28aa13b480a3d8ac419c2

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(call all-named-subdir-makefiles, akmd_free libcamera libsensors)
 #Android makefile to build kernel as a part of Android Build
 #Its here and not under gaosp-kernel/ for compatiblity with the froyo branch
 #The config for the kernel is arch/arm/configs/gaosp_msm_defconfig
-LOCAL_PATH := $(call my-dir)/../../gaosp-kernel
+LOCAL_PATH := $(call my-dir)/../../cm-kernel
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
@@ -18,4 +18,4 @@ $(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/gaosp_msm_defconfig | $(ACP)
 	$(transform-prebuilt-to-target)
 
 $(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
-	$(MAKE) -C gaosp-kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- INSTALL_MOD_STRIP=1
+	$(MAKE) -C cm-kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- INSTALL_MOD_STRIP=1
