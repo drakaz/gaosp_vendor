64409f31c826b6b6f45d5e1f2133ce773c9e8c38
drakaz <drakpub@gmail.com>
2011-03-30 10:27:09 +0200

    Fix sensors module name
    Change-Id: I0d96237ab9fcf9497f130dd4ea44de3dc369c39d

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -21,7 +21,7 @@ ifneq ($(TARGET_SIMULATOR),true)
 # hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := sensors.I7500
+LOCAL_MODULE := sensors.msm7k
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
