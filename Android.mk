79feaef7c9f693d067c38b755394a0e516104338
bukington <bukington@gmail.com>
2011-04-23 22:38:04 +0200

    Force kernel build for each build
    Change-Id: I89458413fc40c231829240575fef72f3e7c40abb

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ include $(call all-named-subdir-makefiles, libaudio libcamera libsensors)
 LOCAL_PATH := $(call my-dir)/../../cm-kernel
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
-TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
+TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage build_kernel
 
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 	$(transform-prebuilt-to-target)
@@ -17,9 +17,8 @@ $(KERNEL_OUT):
 $(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/gaosp_msm_defconfig | $(ACP)
 	$(transform-prebuilt-to-target)
 
-$(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
+build_kernel: $(KERNEL_OUT) $(KERNEL_CONFIG)
 	$(MAKE) -C cm-kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- INSTALL_MOD_STRIP=1
-
 	$(ACP) $(KERNEL_OUT)/drivers/net/wireless/bcm4329/bcm4329.ko $(TARGET_OUT)/lib/modules/
 	$(ACP) $(KERNEL_OUT)/drivers/dpram/dpram.ko $(TARGET_OUT)/lib/modules/
 	$(ACP) $(KERNEL_OUT)/drivers/dpram/multipdp.ko $(TARGET_OUT)/lib/modules/
