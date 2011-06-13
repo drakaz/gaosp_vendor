efae659dd25f0599b2d466368fbe56feeb338e72
Obihoernchen <obihoernchende@gmail.com>
2011-06-13 20:30:27 +0200

    Fix gingerbread repo syncing.

--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 curl http://android.git.kernel.org/repo > repo
 chmod 755 repo
-./repo init -u git://github.com/drakaz/android.git -b gaosp-froyo
+./repo init -u git://github.com/drakaz/android.git -b gaosp-gingerbread
 ./repo sync
